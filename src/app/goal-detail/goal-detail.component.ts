636d0dcc562c86d11f5d4c0506124b89877a3f55
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-07 11:41:59 +0300

    made some changes; added buttons that toggle the visibility of the details of particular goals

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Goals } from '../goals';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Goals } from "../goals";
 
 @Component({
-  selector: 'app-goal-detail',
-  templateUrl: './goal-detail.component.html',
-  styleUrls: ['./goal-detail.component.css']
+  selector: "app-goal-detail",
+  templateUrl: "./goal-detail.component.html",
+  styleUrls: ["./goal-detail.component.css"]
 })
 export class GoalDetailComponent implements OnInit {
-
   @Input() goal: Goals;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit() {
+  goalComplete(complete: boolean) {
+    this.isComplete.emit(complete);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
