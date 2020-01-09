5716bf58d42d934bdb76a729ab2096a8131e6200
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-09 10:10:51 +0300

    final changes

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Goals } from "../goals";
 
 @Component({
-  selector: 'app-goal-form',
-  templateUrl: './goal-form.component.html',
-  styleUrls: ['./goal-form.component.css']
+  selector: "app-goal-form",
+  templateUrl: "./goal-form.component.html",
+  styleUrls: ["./goal-form.component.css"]
 })
 export class GoalFormComponent implements OnInit {
+  newGoal = new Goals(0, "", "", new Date());
 
-  constructor() { }
+  @Output() addGoal = new EventEmitter<Goals>();
 
-  ngOnInit() {
+  submitGoal() {
+    this.addGoal.emit(this.newGoal);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
